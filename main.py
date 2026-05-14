12fa7a6c2b9785739b51626dba29df997cf5492c
Manfred Engström <manfred.engstroom@gmail.com>
2026-05-14 13:14:06 +0200

    Fixat main, tog bort chmod

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from packages.Elliot import UI_CO_REG
-from chmod_on_startup_app import run_mac_startupp
 import sys
-
-
-# if sys.platform == 'darwin': ### Mac: needs to do chmod commands for used binary files
-#     run_mac_startupp()
-
 
 UI_CO_REG.start()
